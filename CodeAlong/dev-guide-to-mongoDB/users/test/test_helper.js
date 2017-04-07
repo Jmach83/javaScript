9f91750dc080b6ae71e64569e15c818cd9985847
Jonas Machon <Jonas Machon>
2017-04-07 12:53:59 +0200

    ionic

--- CodeAlong/dev-guide-to-mongoDB/users/test/test_helper.js
+++ CodeAlong/dev-guide-to-mongoDB/users/test/test_helper.js
@@ -1,8 +1,23 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test');//connect to db 
-mongoose.connection
-.once('open', () => console.log('Good to go!'))//promise fortæller hvad der sker
-.on('error', (error) => {                      //ved connection til mongodb
-    console.warn('Warning', error);
+mongoose.Promise = global.Promise;
+
+before((done) => {
+
+    mongoose.connect('mongodb://localhost/users_test');//connect to db 
+    mongoose.connection
+    .once('open', () => { done();})            //done callback, venter på connection
+    .on('error', (error) => {                 // før der kan startes på test   
+        console.warn('Warning', error);
+    });
 });
+
+ //tømmer db collection users før test bliver kørt
+beforeEach((done) => {
+    mongoose.connection.collections.users.drop(() => {
+        //ready to run next test
+        done(); //done gør at testen ikke fortsætter før db er tømt
+    }); 
+});             
+
+//before udføres 1 gang, beforeEach udføres efter hver test ('Hooks')                                
--- CodeAlong/rest-api-ninja/routes/api.js
+++ CodeAlong/rest-api-ninja/routes/api.js
@@ -4,24 +4,39 @@ const Ninja = require('../models/ninja');
 
 // get a list of ninjas from the db
 router.get('/ninjas', function(req, res){
-    res.send({type: 'GET'});
+    Ninja.geoNear({
+        type: 'Point', coordinates: [parseFloat(req.query.lng), 
+        parseFloat(req.query.lat)]},
+        {maxDistance: 1000000, spherical: true}
+    ).then(function(ninjas){
+        res.send(ninjas)
+    });
 });
 
 //add a new ninja to the db
-router.post('/ninjas', function(req, res){
+router.post('/ninjas', function(req, res, next){
     Ninja.create(req.body).then(function(ninja){
     res.send(ninja);
-    });
+    }).catch(next);
 });
 
 //update a ninja in the db
 router.put('/ninjas/:id', function(req, res){
-    res.send({type: 'PUT'});
+    Ninja.findByIdAndUpdate({_id: req.params.id}, req.body)
+    .then(function(){
+        Ninja.findOne({ _id: req.params.id})
+        .then(function(ninja){
+            res.send(ninja);
+        });
+    });
 });
 
 //delete a ninja from the db
 router.delete('/ninjas/:id', function(req, res){
-    res.send({type: 'DELETE'});
+    Ninja.findByIdAndRemove({_id: req.params.id})
+    .then(function(ninja){
+        res.send(ninja);
+    });
 });
 
 module.exports = router;
